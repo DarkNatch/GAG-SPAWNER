e365d5d75767e1bb4d101b53a6d87ceaf93ed2e3
DarkNatch <jeckonifuji@gmail.com>
2025-06-08 22:30:11 +0800

    about_us.lua

---
+++ about_us.lua
@@ -0,0 +1,10 @@
+return function(Window)
+    local Tab = Window:MakeTab({Name = "About Us 📢", Icon = "rbxassetid://123459", PremiumOnly = false})
+
+    Tab:AddParagraph("Creator", "Script by DarkNatch")
+    Tab:AddParagraph("Socials", [[
+Discord: https://discord.gg/yourlink
+YouTube: https://youtube.com/@yourchannel
+Twitter: https://twitter.com/yourhandle
+]])
+end
