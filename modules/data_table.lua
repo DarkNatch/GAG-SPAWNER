9bfcbf9be0cab228ad1ee8eb1bb368be30ad906e
DarkNatch <jeckonifuji@gmail.com>
2025-06-08 22:41:23 +0800

    data_table.lua

--- modules/data_table.lua
+++ modules/data_table.lua
@@ -1,1 +1,16 @@
+-- modules/data_table.lua
+return function(Window)
+    local Tab = Window:MakeTab({
+        Name = "Data Table 📊",
+        Icon = "rbxassetid://123458",
+        PremiumOnly = false
+    })
 
+    Tab:AddParagraph("Overview", [[
+• Pets spawn as colorful cubes with weight names.
+• Seeds spawn as green blocks with type name.
+• All spawns appear near the player.
+    ]])
+
+    Tab:AddLabel("Note: Data Table styled for black background, white Arial font.")
+end
